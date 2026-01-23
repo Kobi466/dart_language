62dea257fee481da89820d11907b42986c095b65
Kobi466 <loint.24ite@vku.udn.vn>
2026-01-23 19:34:18 +0700

    feat: Refactor Todo feature with Provider state management
    - Restructure project by moving `todo_model.dart` and `todo_service.dart` into `models` and `services` directories respectively.
    - Add the `provider` package to `pubspec.yaml` for state management.
    - Introduce `TodoProvider` to manage the state of the todo list, including loading status and error messages.
    - Implement error handling with a try-catch block in `TodoService`.
    - Create a new `TodoScreen` widget that uses `TodoProvider` to fetch and display the list of todos.
    - Refactor `main.dart` to use `MultiProvider` to make `TodoProvider` available to the widget tree.
    - Delete the old stateful widget implementation from `main.dart` and create a separate `app.dart` file for the root widget.
    - Add `devtools_options.yaml` to configure DevTools settings.

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,15 @@
-import 'package:first_app/todo_model.dart';
-import 'package:first_app/todo_service.dart';
-import 'package:flutter/material.dart';
+import 'package:first_app/app.dart';
+import 'package:first_app/providers/todo_provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "API Demo",
-      debugShowCheckedModeBanner: false,
-      home: _TodoScreen(),
-    );
-  }
-}
-class _TodoScreen extends StatefulWidget{
-  @override
-  State<_TodoScreen> createState() {
-    return _TodoState();
-  }
-}
-
-class _TodoState extends State<_TodoScreen>{
-  bool _loading = true;
-  String? errorMessage;
-  List<Todo> todos = [];
-  @override
-  void initState() {
-    super.initState();
-    loadTodos();
-  }
-
-  Future<void> loadTodos() async{
-    try {
-      final data = await TodoService.fetchTodos();
-      todos = data;
-    } catch (e) {
-      errorMessage = e.toString();
-    } finally {
-      _loading = false;
-      setState(() {
-
-      });
-    }
-  }
-  @override
-  Widget build(BuildContext context) {
-    //loading
-    if (_loading) {
-      return const Scaffold(
-        body: Center(child: CircularProgressIndicator()),
-      );
-    }  
-    //error
-    if (errorMessage != null) {
-      return Scaffold(
-        appBar: AppBar(title: const Text("Todos"),),
-        body: Text(
-          errorMessage!,
-          style: TextStyle(
-            color: Colors.red
-          ),
-        )
-      );
-    }
-
-    //data
-    return Scaffold(
-      appBar: AppBar(title: const Text('Todo List')),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          final todo = todos[index];
-          return ListTile(
-            leading: Icon(
-              todo.completed ? Icons.check_circle
-                  : Icons.circle_outlined,
-              color: todo.completed ? Colors.green
-                  : Colors.grey,
-            ),
-            title: Text(
-              todo.title,
-            ),
-          );
-        },
-      ),
-    );
-  }
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => TodoProvider())
+      ],
+      child: MyApp(),
+    )
+  );
 }
---
+++ lib/services/todo_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:first_app/models/todo_model.dart';
+import 'package:http/http.dart' as http;
+
+class TodoService {
+  static const String baseUrl = "https://jsonplaceholder.typicode.com/todos";
+  static Future<List<Todo>> fetchTodos() async{
+    try{
+      final response  = await http.get(Uri.parse(baseUrl));
+      if (response.statusCode == 200) {
+        final List data = jsonDecode(response.body);
+        return data.map(
+                (e) => Todo.formJson(e)
+        ).toList();
+      } else {
+        throw Exception("Failed to load todos");
+      }
+    }catch(e){
+      print(e.toString());
+      return [];
+    }
+  }
+}
